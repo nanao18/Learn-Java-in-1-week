75f10c28caea88ff4670060dbc65e590abcaea2d
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:17:34 +0900

    練習問題：条件分岐

--- problem3/Problem3_10.java
+++ problem3/Problem3_10.java
@@ -3,9 +3,9 @@ package problem3;
 public class Problem3_10 {
 
 	public static void main(String[] args) {
-		int a = (int)(Math.random()*100)+1;
-		System.out.println("a="+a);
-		if(a <= 10 || a >= 90){
+		int num = (int)(Math.random()*100)+1;
+		System.out.println(num);
+		if(num <= 10 || num >= 90){
 			System.out.println("10以下か90以上の値です");
 		} else {
 			System.out.println("10より大きく90未満です");
