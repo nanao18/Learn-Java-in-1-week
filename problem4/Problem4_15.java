f0532a6d4ee7894b886122a73159e9af2ba92b59
nanao18 <masuworkpg@gmail.com>
2024-06-10 16:06:06 +0900

    練習問題：繰り返し処理

---
+++ problem4/Problem4_15.java
@@ -0,0 +1,14 @@
+package problem4;
+
+public class Problem4_15{
+
+	public static void main(String[] args) {
+		for(int i=1; i<=100; i++) {
+			if(i%10 == 0) {
+				System.out.println(i);
+			} else {
+				System.out.print(i +" ");
+			}
+		}
+	}
+}
