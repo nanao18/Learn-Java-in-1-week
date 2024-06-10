49e4b5af74ed2c626340906527367d23b0f316e5
nanao18 <masuworkpg@gmail.com>
2024-06-10 16:47:58 +0900

    練習問題：配列変数

--- problem5/Problem5_03.java
+++ problem5/Problem5_03.java
@@ -3,23 +3,23 @@ package problem5;
 public class Problem5_03{
 
 	public static void main(String[] args) {
-		int a[] = new int[10];
+		int data[] = new int[10];
 		for(int i=0; i<a.length; i++) {
-			a[i] = (int)(Math.random()*100)+1;
-			System.out.print(a[i] + " ");
+			data[i] = (int)(Math.random()*100)+1;
+			System.out.print(data[i] + " ");
 		}
 		System.out.println();
 		System.out.print("奇数 : ");
 		for(int i=0; i<a.length; i++) {
-			if(a[i]%2 != 0) {
-				System.out.print(a[i] + " ");
+			if(data[i]%2 != 0) {
+				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println();
 		System.out.print("偶数 : ");
 		for(int i=0; i<a.length; i++) {
-			if(a[i]%2 == 0) {
-				System.out.print(a[i] + " ");
+			if(data[i]%2 == 0) {
+				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println()
