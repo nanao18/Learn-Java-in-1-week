f9a6fe9f083c24733280bef8e7878a3021b95f8e
nanao18 <masuworkpg@gmail.com>
2024-06-10 14:34:10 +0900

    練習問題：条件分岐

---
+++ problem3/Problem3_3.java
@@ -0,0 +1,12 @@
+package problem3;
+
+public class Problem3_3 {
+
+	public static void main(String[] args) {
+		int dice = (int)(Math.random()*100)+1;
+		System.out.println("a="+a);
+		if(a < 50){
+			System.out.println("50未満です");
+		}
+	}
+}
