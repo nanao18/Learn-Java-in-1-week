875a5ca2cdfa4761cdfc195861e1f117e8d16c0d
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:00:43 +0900

    練習問題：条件分岐

--- problem3/Problem3_05.java
+++ problem3/Problem3_05.java
@@ -3,7 +3,7 @@ package problem3;
 public class Problem3_5 {
 
 	public static void main(String[] args) {
-		int dice = (int)(Math.random()*100)+1;
+		int a = (int)(Math.random()*100)+1;
 		System.out.println("a="+a);
 		if(a >= 20 || a < 90){
 			System.out.println("20以上80未満です");
