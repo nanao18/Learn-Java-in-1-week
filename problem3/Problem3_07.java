6686eb377e7811d2392f9cbb07dd32d77931b147
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:02:57 +0900

    練習問題：条件分岐

--- problem3/Problem3_07.java
+++ problem3/Problem3_07.java
@@ -3,7 +3,7 @@ package problem3;
 public class Problem3_7 {
 
 	public static void main(String[] args) {
-		int dice = (int)(Math.random()*10)+1;
+		int a = (int)(Math.random()*10)+1;
 		System.out.println("a="+a);
 		if(a >= 5){
 			System.out.println("5以上です");
