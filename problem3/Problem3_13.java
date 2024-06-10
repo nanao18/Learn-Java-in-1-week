0cee3cd2667b774180c6d51bd08d8c43af794d56
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:19:01 +0900

    練習問題：条件分岐

--- problem3/Problem3_13.java
+++ problem3/Problem3_13.java
@@ -3,13 +3,13 @@ package problem3;
 public class Problem3_13 {
 
 	public static void main(String[] args) {
-		int a = (int)(Math.random()*3)+1;
-		System.out.println("a="+a);
-		if(a == 1){
+		int num = (int)(Math.random()*3)+1;
+		System.out.println(num);
+		if(num == 1){
 			System.out.println("グー");
-		} else if(a == 2){
+		} else if(num == 2){
 			System.out.println("パー");
-		} else if(a == 3){
+		} else if(num == 3){
 			System.out.println("チョキ");
 		}
 	}
