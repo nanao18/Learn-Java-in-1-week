86ae862f4d1746ca3a3efa9b3dd96cb8234475dc
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:43:54 +0900

    練習問題：繰り返し処理

---
+++ problem4/Problem4_06.java
@@ -0,0 +1,12 @@
+package problem4;
+
+public class Problem4_06 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		while(a >= 0) {
+			System.out.println("a = "+ a);
+			a--;
+		}
+	}
+}
