4faf59de4eed0c7312526aeae19829bce08f147d
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:56:31 +0900

    練習問題：繰り返し処理

--- problem4/Problem4_11.java
+++ problem4/Problem4_11.java
@@ -4,7 +4,7 @@ public class Problem4_11 {
 
 	public static void main(String[] args) {
 		int max = 0;
-		for(int i=1; a<=5; i++) {
+		for(int i=1; i<=5; i++) {
 			int num = (int)(Math.random()*100)+1;
 			System.out.println(num);
 			if(num > max) {
