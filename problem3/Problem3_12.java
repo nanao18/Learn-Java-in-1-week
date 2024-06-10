21dd33870bdc50ede37dec785d35feb9dc1b9e9f
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:18:29 +0900

    練習問題：条件分岐

--- problem3/Problem3_12.java
+++ problem3/Problem3_12.java
@@ -3,11 +3,11 @@ package problem3;
 public class Problem3_12 {
 
 	public static void main(String[] args) {
-		int a = (int)(Math.random()*10)-10;
-		System.out.println("a="+a);
-		if(a < 0){
+		int num = (int)(Math.random()*10)-10;
+		System.out.println(num);
+		if(num < 0){
 			System.out.println("負の値です");
-		} else if(a > 0){
+		} else if(num > 0){
 			System.out.println("正の値です");
 		} else {
 			System.out.println("0です");
