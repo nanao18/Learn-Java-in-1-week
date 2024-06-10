089844ed600d0a5ad7fe6d17b59cc895aa78665a
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:16:04 +0900

    練習問題：条件分岐

--- problem3/Problem3_07.java
+++ problem3/Problem3_07.java
@@ -3,9 +3,9 @@ package problem3;
 public class Problem3_7 {
 
 	public static void main(String[] args) {
-		int a = (int)(Math.random()*10)+1;
-		System.out.println("a="+a);
-		if(a >= 5){
+		int num = (int)(Math.random()*10)+1;
+		System.out.println(num);
+		if(num >= 5){
 			System.out.println("5以上です");
 		} else {
 			System.out.println("5未満です");
