fef30915191c43111d2eb71a08f5921bd4d33be3
nanao18 <masuworkpg@gmail.com>
2024-06-10 16:00:06 +0900

    練習問題：繰り返し処理

---
+++ problem4/Problem4_13.java
@@ -0,0 +1,15 @@
+package problem4;
+
+public class Problem4_13{
+
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*100)+1;
+		if(num < 5) {
+			System.out.println("発生した数値："+ num);
+		} else {
+			for(int i=1; i<=num; i++) {
+				System.out.print("★");
+			}
+		}
+	}
+}
