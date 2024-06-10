03c4ffb2277fd2198a0fdc52751730014db7b708
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:16:42 +0900

    練習問題：条件分岐

--- problem3/Problem3_08.java
+++ problem3/Problem3_08.java
@@ -3,9 +3,9 @@ package problem3;
 public class Problem3_8 {
 
 	public static void main(String[] args) {
-		int a = (int)(Math.random()*10)+1;
-		System.out.println("a="+a);
-		if(a != 1){
+		int num = (int)(Math.random()*10)+1;
+		System.out.println(num);
+		if(num != 1){
 			System.out.println("１ではありません");
 		} else {
 			System.out.println("1です。");
