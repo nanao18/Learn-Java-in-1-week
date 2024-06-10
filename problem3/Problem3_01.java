5fdd4632bfa447a15a8410b33fe197ed753e5539
nanao18 <masuworkpg@gmail.com>
2024-06-10 14:59:03 +0900

    練習問題：条件分岐

--- problem3/Problem3_01.java
+++ problem3/Problem3_01.java
@@ -3,8 +3,7 @@ package problem3;
 public class Problem3_1 {
  
 	public static void main(String[] args) {
-		//1から10までの乱数を発生させる
-		int dice = (int)(Math.random()*10)+1;
+		int a = (int)(Math.random()*10)+1;
 		System.out.println("a="+a);
 		if(a >= 5){
 			System.out.println("5以上です");
