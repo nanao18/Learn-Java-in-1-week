f74c9ec5c2115a81a4c7332d668e6b972efe5784
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:23:50 +0900

    練習問題：条件分岐

---
+++ problem3/Problem3_18.java
@@ -0,0 +1,16 @@
+package problem3;
+
+public class Problem3_18 {
+
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*35)-10;
+		System.out.println("摂氏"+ num +"度");
+		if(num >= 30){
+			System.out.println("真夏日です。");
+		else if(num >= 25){
+			System.out.println("夏日です。");
+		} else if(num < 0){
+			System.out.println("真冬日です。");
+		}
+	}
+}
