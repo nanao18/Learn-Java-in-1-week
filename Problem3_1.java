9378520ae318da609ba463e4756d872680c8535b
nanao18 <masuworkpg@gmail.com>
2024-06-10 14:30:22 +0900

    練習問題：条件分岐

---
+++ Problem3_1.java
@@ -0,0 +1,12 @@
+package problem3;
+ 
+public class Problem3_1 {
+ 
+	public static void main(String[] args) {
+		//1から10までの乱数を発生させる
+		int dice = (int)(Math.random()*10)+1;
+		if(a >= 5){
+			System.out.println("5以上です");
+		}
+	}
+}
