c8a88f2a75924ad447eae5728acdbf379a2d997d
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:56:13 +0900

    練習問題：繰り返し処理

--- problem4/Problem4_10.java
+++ problem4/Problem4_10.java
@@ -3,7 +3,7 @@ package problem4;
 public class Problem4_10 {
 
 	public static void main(String[] args) {
-		for(int i=1; a<=5; i++) {
+		for(int i=1; i<=5; i++) {
 			int num = (int)(Math.random()*100)+1;
 			System.out.println(num);
 		}
