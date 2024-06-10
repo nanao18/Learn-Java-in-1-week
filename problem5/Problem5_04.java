e42833d6cb46b880d9288e61829c504519219d7c
nanao18 <masuworkpg@gmail.com>
2024-06-10 16:47:00 +0900

    練習問題：配列変数

--- problem5/Problem5_04.java
+++ problem5/Problem5_04.java
@@ -3,7 +3,7 @@ package problem5;
 public class Problem5_04{
 
 	public static void main(String[] args) {
-		int data[] = new int[10];
+		int data[] = new int[10];	//大きさは実行結果の例に合わせている
 		for(int i=0; i<a.length; i++) {
 			data[i] = (int)(Math.random()*10)+1;
 			System.out.print(data[i] + " ");
