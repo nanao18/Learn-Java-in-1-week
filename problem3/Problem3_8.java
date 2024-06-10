2f25807de02b09a640e21e5bbef6ed76d9fbff03
nanao18 <masuworkpg@gmail.com>
2024-06-10 14:43:29 +0900

    練習問題：条件分岐

---
+++ problem3/Problem3_8.java
@@ -0,0 +1,14 @@
+package problem3;
+
+public class Problem3_8 {
+
+	public static void main(String[] args) {
+		int dice = (int)(Math.random()*10)+1;
+		System.out.println("a="+a);
+		if(a != 1){
+			System.out.println("１ではありません");
+		} else {
+			System.out.println("1です。");
+		}
+	}
+}
