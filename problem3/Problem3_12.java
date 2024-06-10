c3eae3931f1a3c038dcfb24410f8599bbbaf6592
nanao18 <masuworkpg@gmail.com>
2024-06-10 15:05:12 +0900

    練習問題：条件分岐

--- problem3/Problem3_12.java
+++ problem3/Problem3_12.java
@@ -3,7 +3,7 @@ package problem3;
 public class Problem3_12 {
 
 	public static void main(String[] args) {
-		int dice = (int)(Math.random()*10)-10;
+		int a = (int)(Math.random()*10)-10;
 		System.out.println("a="+a);
 		if(a < 0){
 			System.out.println("負の値です");
