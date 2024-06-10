55bf56423ed54a053bc51b6407091b345f493984
nanao18 <masuworkpg@gmail.com>
2024-06-10 14:41:15 +0900

    練習問題：条件分岐

---
+++ problem3/Problem3_6.java
@@ -0,0 +1,12 @@
+package problem3;
+
+public class Problem3_6 {
+
+	public static void main(String[] args) {
+		int dice = (int)(Math.random()*6)+1;
+		System.out.println("a="+a);
+		if(a >= 3){
+			System.out.println("3以上です。");
+		}
+	}
+}
