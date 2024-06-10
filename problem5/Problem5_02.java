96425b03b766cc24a0b2bc8d91a465add1a68edf
nanao18 <masuworkpg@gmail.com>
2024-06-10 16:38:48 +0900

    練習問題：配列変数

---
+++ problem5/Problem5_02.java
@@ -0,0 +1,11 @@
+package problem5;
+
+public class Problem5_02{
+
+	public static void main(String[] args) {
+		double d[] = {0.2, -5.1, 3.2, 1.8};
+		for(int i=0; i<d.length; i++) {
+			System.out.print("d[" + i +"]=" + d[i] + " ");
+		}
+	}
+}
