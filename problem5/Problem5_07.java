810be31f43e5c627376cec62b1e3071cb2256431
nanao18 <masuworkpg@gmail.com>
2024-06-12 15:36:40 +0900

    練習問題：配列変数

---
+++ problem5/Problem5_07.java
@@ -0,0 +1,35 @@
+package problem5;
+
+public class Problem5_07{
+
+	public static void main(String[] args) {
+		int data[] = new int[5];
+		int sum;
+		int avg;
+		for(int i=0; i<a.length; i++) {
+			data[i] = (int)(Math.random()*10)+1;
+			System.out.print(data[i] + " ");
+			sum += data[i];
+		}
+		avg = sum / data.length;
+
+		System.out.println();
+		System.out.println("合計値 : " + sum);
+		System.out.println("平均値 : " + avg);
+
+		System.out.println();
+		System.out.print("平均値より大きい数 : ");
+		for(int i=0; i<a.length; i++) {
+			if(data[i] > avg) {
+				System.out.print(data[i] + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("平均値より小さい数 : ");
+		for(int i=0; i<a.length; i++) {
+			if(data[i] < avg) {
+				System.out.print(data[i] + " ");
+			}
+		}
+	}
+}
