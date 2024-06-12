154589ccdc621ec3e8f3949569445763f44b96e6
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:13:34 +0900

    練習問題：配列変数

--- problem5/Problem5_03.java
+++ problem5/Problem5_03.java
@@ -4,20 +4,20 @@ public class Problem5_03{
 
 	public static void main(String[] args) {
 		int data[] = new int[10];
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			data[i] = (int)(Math.random()*100)+1;
 			System.out.print(data[i] + " ");
 		}
 		System.out.println();
 		System.out.print("奇数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i]%2 != 0) {
 				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println();
 		System.out.print("偶数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i]%2 == 0) {
 				System.out.print(data[i] + " ");
 			}
