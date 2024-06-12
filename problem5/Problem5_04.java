2709554e12b0c2d050f1675b0c1e6f7f08db1b16
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:13:51 +0900

    練習問題：配列変数

--- problem5/Problem5_04.java
+++ problem5/Problem5_04.java
@@ -5,20 +5,20 @@ public class Problem5_04{
 	public static void main(String[] args) {
 		//実行結果の例に合わせて、「大きさは、10」「値は、1から100の乱数」
 		int data[] = new int[10];
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			data[i] = (int)(Math.random()*100)+1;
 			System.out.print(data[i] + " ");
 		}
 		System.out.println();
 		System.out.print("50以上の数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i] >= 50) {
 				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println();
 		System.out.print("50未満の数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i] < 50) {
 				System.out.print(data[i] + " ");
 			}
