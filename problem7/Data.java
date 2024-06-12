52ec87491ce2d8d9e3c4020a3094e7a6bebc3419
nanao18 <masuworkpg@gmail.com>
2024-06-12 17:41:05 +0900

    練習問題：アクセス指定子とカプセル化

---
+++ problem7/Data.java
@@ -0,0 +1,23 @@
+package problem7;
+ 
+public class Data {
+	//	メンバ変数number
+	private int number = 0;
+	//	メンバ変数comment
+	private String comment = "";
+
+	public int getNumber(){
+		return this.number;
+	}
+	public void setNumber(int number){
+		this.number = number;
+	}
+
+	public String getComment(){
+		return this.comment;
+	}
+	public void setComment(String comment){
+		this.comment = comment;
+	}
+
+}
