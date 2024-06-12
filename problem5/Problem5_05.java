d2801f5240efdd3ced1a10a8cfdcdc20b9581365
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:14:09 +0900

    練習問題：配列変数

--- problem5/Problem5_05.java
+++ problem5/Problem5_05.java
@@ -4,20 +4,20 @@ public class Problem5_05{
 
 	public static void main(String[] args) {
 		int data[] = new int[10];
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			data[i] = (int)(Math.random()*10)+1;
 			System.out.print(data[i] + " ");
 		}
 		System.out.println();
 		System.out.print("3の倍数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i]%3 == 0) {
 				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println();
 		System.out.print("3の倍数以外の数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i]%3 != 0) {
 				System.out.print(data[i] + " ");
 			}
