72c541b71a5f3d9b415d9535ca84bfdc99f891ca
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:29:52 +0900

    練習問題：配列変数

---
+++ problem5/Problem5_13.java
@@ -0,0 +1,22 @@
+package problem5;
+
+public class Problem5_13{
+
+	public static void main(String[] args) {
+		int data[][] = new int[3][3];
+		int max;
+		int min;
+		for(int i=0; i<data.length; i++) {
+			for(int j=0; j<data[i].length; j++) {
+				data[i][j] = (int)(Math.random()*9);
+				System.out.print(data[i][j] + " ");
+				max = Math.max(max, data[i][j]);
+				min = Math.min(min, data[i][j]);
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println("最大値：" + max);
+			System.out.println("最小値：" + min);
+		}
+	}
+}
