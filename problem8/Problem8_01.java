3750a7999d806f881bea9da63f806c5d4b89a6bc
nanao18 <masuworkpg@gmail.com>
2024-06-12 20:25:20 +0900

    練習問題：チャレンジ問題

--- problem8/Problem8_01.java
+++ problem8/Problem8_01.java
@@ -4,11 +4,12 @@ public class Problem8_01{
 
 	public static void main(String[] args) {
 		//ピタゴラス数（Pythagorean triple）重複あり
-	for(int a=1; a<=100; a++) {
-		for(int b=1; b<=100; b++) {
-			for(int c=1; c<=100; c++) {
-				if(a*a + b*b == c*c) {
-					System.out.println("("+ a +","+ b +","+ c +")");
+		for(int a=1; a<=100; a++) {
+			for(int b=1; b<=100; b++) {
+				for(int c=1; c<=100; c++) {
+					if(a*a + b*b == c*c) {
+						System.out.println("("+ a +","+ b +","+ c +")");
+					}
 				}
 			}
 		}
