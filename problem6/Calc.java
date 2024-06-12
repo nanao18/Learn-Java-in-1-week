709241cea207854f0142cce683f6f423b129c145
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:46:52 +0900

    練習問題：クラスとオブジェクト

---
+++ problem6/Calc.java
@@ -0,0 +1,20 @@
+package problem6;
+ 
+public class Calc {
+    //  加算
+    double add(double a,double b){
+        return a + b;
+    }
+    //  加算
+    double sub(double a,double b){
+        return a - b;
+    }
+    //  積算
+    double sub(double a,double b){
+        return a * b;
+    }
+    //  割算
+    double sub(double a,double b){
+        return a / b;
+    }
+}
