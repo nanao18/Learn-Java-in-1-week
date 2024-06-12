8895fe00fe57e71bcd51c9537f2076ea004f4f1f
nanao18 <masuworkpg@gmail.com>
2024-06-12 16:15:18 +0900

    練習問題：配列変数

--- problem5/Problem5_09.java
+++ problem5/Problem5_09.java
@@ -4,7 +4,7 @@ public class Problem5_09{
 
 	public static void main(String[] args) {
 		int data[] = new int[10];
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			data[i] = (int)(Math.random()*100)+1;
 			System.out.print(data[i] + " ");
 		}
@@ -12,14 +12,14 @@ public class Problem5_09{
 
 		System.out.println();
 		System.out.print("50以上の数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i] >= 50) {
 				System.out.print(data[i] + " ");
 			}
 		}
 		System.out.println();
 		System.out.print("50未満の数 : ");
-		for(int i=0; i<a.length; i++) {
+		for(int i=0; i<data.length; i++) {
 			if(data[i] < 50) {
 				System.out.print(data[i] + " ");
 			}
