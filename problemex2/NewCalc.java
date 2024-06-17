4a1346d789e5f1adb13c0f9787867fee5b71cdac
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:42:25 +0900

    練習問題：継承

---
+++ problemex2/NewCalc.java
@@ -0,0 +1,13 @@
+package problemex2;
+ 
+public class NewCalc extends FundCalc {
+ 
+	public double mul()
+	{
+		 return number1 * number2;
+	}
+	public double div()
+	{
+		 return number1 / number2;
+	}
+}
