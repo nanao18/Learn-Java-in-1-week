3c61db63a04682ea426b88cc8bf97530b0668c06
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:06:16 +0900

    Stringクラス

---
+++ class4/ClassSample04.java
@@ -0,0 +1,12 @@
+package class4;
+ 
+public class SampleClass04 {
+ 
+	public static void main(String[] args) {
+		String str = "Apple";
+		System.out.println(str.toUpperCase());			//"APPLE"が出力される
+		System.out.println(str.toLowerCase());			//"apple"が出力される
+ 
+	}
+ 
+}
