b0fc028b8d1eb7e5959cc70a3e5589237b411162
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:04:55 +0900

    Stringクラス

---
+++ class4/ClassSample01.java
@@ -0,0 +1,13 @@
+package class4;
+ 
+public class SampleClass01 {
+ 
+	public static void main(String[] args) {
+		String s1,s2;
+		s1 = "あいうえお";
+		s2 = "Hello";
+		System.out.println("「"+s1+"」の文字列の長さは"+s1.length()+"です。");
+		System.out.println("「"+s2+"」の文字列の長さは"+s2.length()+"です。");
+	}
+ 
+}
