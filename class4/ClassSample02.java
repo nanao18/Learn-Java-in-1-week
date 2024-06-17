72e1ce63fd801aa6c243f5f029897b92dd4c602d
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:05:27 +0900

    Stringクラス

---
+++ class4/ClassSample02.java
@@ -0,0 +1,12 @@
+package class4;
+ 
+public class SampleClass02 {
+ 
+	public static void main(String[] args) {
+		String str1 = "ABCDEFGHIJKLMNOP";
+		System.out.println(str1.substring(3,8));	//「DEFGH」が出力される
+		System.out.println(str1.substring(3));		//「DEFGHIJKLMNOP」が出力される
+ 
+	}
+ 
+}
