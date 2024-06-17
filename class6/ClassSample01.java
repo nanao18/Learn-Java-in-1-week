40b70a10ee73e4662b0ac738deab5a6e9fdae14b
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:10:51 +0900

    StringBuilderクラス

---
+++ class6/ClassSample01.java
@@ -0,0 +1,12 @@
+package class6;
+ 
+public class SampleClass01 {
+ 
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Apple ");		//オブジェクトを生成
+		sb.append("Banana ");		//	文字列を末尾に追加
+		sb.append("Piniapple"); 	//	文字列を末尾に追加
+		System.out.println(sb); 	//	内容を表示
+	}
+ 
+}
