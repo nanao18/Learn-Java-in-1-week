4cc030473b5a424b2510232f485f7facf06cc3b1
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:06:41 +0900

    Stringクラス

---
+++ class4/ClassSample05.java
@@ -0,0 +1,11 @@
+package class4;
+ 
+public class SampleClass05 {
+ 
+	public static void main(String[] args) {
+		String str1 = "     Hello World.     "; // 空白を含んだ文字列先頭と末尾に空白
+		System.out.println(str1.trim());		// 末尾と先頭の空白が削除されてが出力される
+ 
+	}
+ 
+}
