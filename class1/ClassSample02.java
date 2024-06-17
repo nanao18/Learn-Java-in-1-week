f3fa05579f08e6ee2811a5ad13e7cd7e61886b95
nanao18 <masuworkpg@gmail.com>
2024-06-17 15:39:33 +0900

    ファイル関連クラス

---
+++ class1/ClassSample02.java
@@ -0,0 +1,20 @@
+package class1;
+ 
+import java.io.*;
+ 
+public class ClassSample02 {
+	public static void main(String[] args){
+		File fi = new File("c:¥¥test");
+		File[] fiList = fi.listFiles();
+		System.out.println("指定したディレクトリは、" + fi.getAbsolutePath() + "です。");
+		for(int i = 0; i < fiList.length; i++){
+			if(fiList[i].isFile() == true){
+				System.out.println("ファイル名は、" + fiList[i].getName() + "です。");
+			}else if(fiList[i].isDirectory() == true){
+				System.out.println("ディレクトリ名は、" + fiList[i].getName() + "です。");
+			}
+		}
+ 
+	}
+ 
+}
