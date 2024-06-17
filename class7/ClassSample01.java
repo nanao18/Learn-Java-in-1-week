9dd3c857f9919033db157af4ad84595880dbe8a7
nanao18 <masuworkpg@gmail.com>
2024-06-17 16:14:13 +0900

    ロケールと数値・通貨

---
+++ class7/ClassSample01.java
@@ -0,0 +1,22 @@
+package class7;
+ 
+import java.util.*;
+ 
+public class SampleClass01 {
+ 
+	public static void main(String[] args) {
+		//各言語と国のロケールを取得
+		Locale jp = new Locale("ja","JP");	//日本のロケールを生成
+		Locale us = new Locale("en","US");		//アメリカのロケールを生成
+		Locale ch = new Locale("zh","CN");	//中国のロケールを生成
+ 
+		//各ロケールの言語と国名を出力
+		System.out.println("日本のロケール 言語 : "
+		+ jp.getDisplayLanguage() + " 国名 : " + jp.getDisplayCountry());
+		System.out.println("アメリカのロケール 言語 : "
+		+ us.getDisplayLanguage() + " 国名 :" + us.getDisplayCountry());
+		System.out.println("中国のロケール 言語 : "
+		+ ch.getDisplayLanguage() + " 国名 : " + ch.getDisplayCountry() );
+	}
+ 
+}
