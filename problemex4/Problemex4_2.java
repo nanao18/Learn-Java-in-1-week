bcb3a69046490e82ad2d1b56ff3ef8b9842a81ad
nanao18 <masuworkpg@gmail.com>
2024-06-18 14:46:58 +0900

    練習問題：インターフェース

---
+++ problemex4/Problemex4_2.java
@@ -0,0 +1,21 @@
+package problemex4;
+ 
+public class Problemex4_2 {
+ 
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		AlarmClock ac = new AlarmClock();	//	アラーム付き時計クラスのインスタンスを生成
+	}
+	//	アラームとしての処理
+	public static void funcAlarm(IAlarm alarm){
+		alarm.setAlarm();	//	アラームのセット
+		alarm.alarm();		//	アラームを鳴らす
+		alarm.stopAlarm();	//	アラームを止める
+	}
+	//	時計としての処理
+	public static void funcClock(IClock clock){
+		clock.adjustTime(); //	時間を調整する
+		clock.showTime();	//	時間を表示する
+	}
+ 
+}
